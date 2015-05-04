20c2d5fe6052ba3188116b5a2e9de9fd2f93f2c2
franknelson <fnelson12@gmail.com>
2015-05-03 21:58:37 -0500

    adjusting the home page and inner page layout

--- page-templates/page-home.php
+++ page-templates/page-home.php
@@ -6,18 +6,40 @@
 
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 			
-		<article class="post" id="post-<?php the_ID(); ?>">
+		<article class="home-page" id="post-<?php the_ID(); ?>">
 
 			<div class="home-entry">
 
 				<?php the_content(); ?>
 
+            
 			</div>
+            <?php
+                    $args = array( 'post_type' => 'project', );
+                    $loop = new WP_Query( $args );
+                    while ( $loop->have_posts() ) : $loop->the_post();
+                      echo '<div class="thumbnail">';
+                        the_post_thumbnail();
+                      echo '</div>';
+                    endwhile;
+                ?>
+<!--
+            <div class ="projects">
+                <?php
+                    $args = array( 'post_type' => 'project', );
+                    $loop = new WP_Query( $args );
+                    while ( $loop->have_posts() ) : $loop->the_post();
+                      echo '<div class="thumbnail">';
+                        the_post_thumbnail();
+                      echo '</div>';
+                    endwhile;
+                ?>
+            </div>
+-->
 
 		</article>
 
 		<?php endwhile; endif; ?>
 
-<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
